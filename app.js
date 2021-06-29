230cf636855fe37f4e7dc7f498807890eec9f700
ikeadeoyin <ikeadeoyin@gmail.com>
2021-06-30 00:51:32 +0100

    made changes

--- app.js
+++ app.js
@@ -30,19 +30,3 @@ app.get("*", checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies',requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-// cookies
-// app.get("/set-cookies", (req,res)=>{
-//     // res.setHeader("Set-Cookie", "newuser=true");
-//     res.cookie("newUser", false)
-//     res.cookie("isEmployee", true, {maxAge:1000 * 60 * 60  *24, httpOnly:true})
-//     res.send("you got the cookie")
-
-// })
-
-// app.get("/read-cookies", (req,res)=>{
-//     const cookies = req.cookies;
-//     console.log(cookies)
-//     res.json(cookies)
-   
-// })
